2345e4cacfb187926f146e9d549d928685d82f32
MahirRaj <85392826+MahirRaj@users.noreply.github.com>
2021-09-24 20:06:18 +0530

    Add files via upload

---
+++ Boat.js
@@ -0,0 +1,27 @@
+class Boat{
+    constructor(x,y,width,height,boatPos){
+        var options = {
+            restitution :  0.7,
+            density : 1.0,
+            friction : 1.0
+        }
+         this.body = Bodies.rectangle(x,y,width,height,options);
+         World.add(world,this.body);
+         this.width = width;
+         this.height = height;
+         this.boatPos = boatPos;
+         this.image = loadImage("assets/boat.png");
+    }
+
+    display(){
+         var pos = this.body.position;
+         var angle=this.body.angle;
+         push();
+         translate(pos.x,pos.y);
+         rotate(angle);
+         imageMode(CENTER);
+         image(this.image,0,this.boatPos,this.width,this.height);
+         pop();
+    }
+
+}
